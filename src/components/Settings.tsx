5b22c1d781e9507deb24746f24e2b1e2c231246f
Mickael JEGAT <moosic39@gmail.com>
2022-12-13 18:01:07 +0100

    working setting

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -5,98 +5,192 @@ import SaveIcon from "@mui/icons-material/Save";
 import { getInfo, putInfo } from "../fetch.js";
 
 function Settings() {
-	// interface Tot {
-	// 	firstName: string;
-	// 	lastName: string;
-	// 	email: string;
-	// 	password: string;
-	// 	doctor: string;
-	// 	doctorEmail: string;
-	// }
+  interface InputProps {
+    // username: string;
+    firstname?: string;
+    lastname?: string;
+    email?: string;
+    password?: string;
+    doctor?: string;
+    doctoremail?: string;
+  }
 
-	const tot = {
-		firstName: "",
-		lastName: "",
-		email: "",
-		password: "",
-		doctor: "",
-		doctorEmail: "",
-	};
+  interface Init {
+    username: string;
+    firstname: string;
+    lastname: string;
+    email: string;
+    hash: string;
+    token: string;
+    doctor: string;
+    doctoremail: string;
+  }
 
-	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const { name, value } = e.target;
+  const input = {
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+    doctor: "",
+    doctoremail: "",
+  };
 
-		setValues({
-			...values,
-			[name]: value,
-		});
-	};
+  const handleInputChange = (e: any) => {
+    const { name, value } = e.target;
 
-	const username: string = window.location.pathname.split("/")[2];
-	React.useEffect(() => {
-		getInfo(username)
-			.then((infos) => {
-				setValues(infos[0]);
-				console.log(values);
-			})
-			.catch((err) => {
-				console.error({ err });
-			});
-	}, []);
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
 
-	const allInput: string[] = Object.keys(tot);
-	const allInputInit = allInput.map((e: string) => "");
-	const [values, setValues] = useState(allInputInit);
-	console.log(values);
+  const username: string = window.location.pathname.split("/")[2];
+  React.useEffect(() => {
+    getInfo(username)
+      .then((infos) => {
+        setInit(infos[0]);
+      })
+      .catch((err) => {
+        console.error({ err });
+      });
+  }, []);
 
-	const element = Object.entries(values).map((e, i) => (
-		<div key={i}>
-			<label htmlFor={e[1]}>
-				<TextField
-					id="outlined-basic"
-					label={e[0]}
-					variant="outlined"
-					onChange={(e) => {
-						handleInputChange(e);
-					}}
-					defaultValue={e[1]}
-					className={"center"}
-				/>
-			</label>
-		</div>
-	));
+  // const allInput: string[] = Object.keys(input);
+  // const allInputInit = allInput.map(() => "");
+  const [values, setValues] = useState<InputProps>(input);
 
-	return (
-		<div>
-			<h2>Settings</h2>
-			<Box
-				component="form"
-				sx={{
-					"& > :not(style)": {
-						m: 1,
-						width: "33ch",
-					},
-				}}
-				noValidate
-				autoComplete="off"
-			>
-				{element}
-			</Box>
-			<Button
-				variant="contained"
-				onClick={() => {
-					putInfo({ ...values }, username)
-						.then((data) => {
-							console.log(data);
-						})
-						.catch((error) => console.error(error));
-				}}
-				endIcon={<SaveIcon />}
-			>
-				Save modifications
-			</Button>
-		</div>
-	);
+  const dbInput: string[] = Object.keys(values);
+  const dbInit = dbInput.map((e) => e);
+  const [init, setInit] = useState<Init>(dbInit);
+
+  // console.log("db", init);
+  // console.log("val", values);
+
+  // const element = Object.entries(values).map((e, i) => (
+  //   <div key={i}>
+  //     <label htmlFor={e[1]}>
+  //       <TextField
+  //         id="outlined-basic"
+  //         label={e[0]}
+  //         variant="outlined"
+  //         onChange={(poulet) => {
+  //           handleInputChange(poulet);
+  //         }}
+  //         defaultValue={e[1]}
+  //         size={"small"}
+  //         className={"center"}
+  //       />
+  //     </label>
+  //   </div>
+  // ));
+
+  const dbElement = Object.entries(init).map((e, i) => (
+    <div key={i}>
+      <label htmlFor={e[0]}>
+        <TextField
+          id="outlined-basic"
+          label={e[0]}
+          variant="outlined"
+          size={"small"}
+          className={"center"}
+          value={e[1]}
+        />
+      </label>
+    </div>
+  ));
+
+  const element = Object.entries(input).map((e, i) => (
+    <div key={i}>
+      <label htmlFor={e[0]}>
+        <TextField
+          id="outlined-basic"
+          label={e[0]}
+          variant="outlined"
+          name={e[0]}
+          onChange={(poulet) => {
+            handleInputChange(poulet);
+          }}
+          size={"small"}
+          className={"center"}
+        />
+      </label>
+    </div>
+  ));
+
+  function sendData() {
+    // let values[_id]='';
+    // console.log(init._id, { ...values });
+    // setValues({ ...values }, ([_id] = init._id));
+    // Object.defineProperty(values, "username", {
+    //   value: init.username,
+    //   writable: true,
+    // });
+
+    putInfo(values, username)
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => console.log({ err }));
+  }
+
+  return (
+    <div>
+      <h2>Settings</h2>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": {
+            m: 1,
+            width: "33ch",
+          },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        {dbElement}
+      </Box>
+      <br />
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": {
+            m: 1,
+            width: "33ch",
+          },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        {element}
+      </Box>
+      <Button
+        variant="contained"
+        onClick={
+          sendData
+          //   () => {
+          //   console.log(values);
+          //
+          //   // if (Object.values(values)) {
+          //   //   const toSend = [Object.keys(values), Object.values(values)];
+          //   //   console.log({ ...toSend });
+          //   // }
+          //   // putInfo({ ...values }, username)
+          //   //   .then((data) => {
+          //   //     console.log(data);
+          //   //   })
+          //   //   .catch((error) => console.error(error));
+          // }
+        }
+        endIcon={<SaveIcon />}
+        size={"small"}
+      >
+        Save modifications
+      </Button>
+      <Button variant={"contained"} color={"error"} size={"small"}>
+        Delete All
+      </Button>
+    </div>
+  );
 }
 
 export default Settings;
--- src/fetch.ts
+++ src/fetch.ts
@@ -1,86 +1,96 @@
+import { useNavigate } from "react-router-dom";
+
 const PATH = "http://127.0.0.1:3000";
 
 function isAuth(): HeadersInit | undefined {
-	const token = window.localStorage.getItem("token");
-	const headers: {
-		"Content-Type": string;
-		token?: string | undefined;
-	} = {
-		"Content-Type": "application/json; charset=UTF-8",
-		token: "",
-	};
-	if (token !== undefined) {
-		headers.token = token;
-	} else {
-		delete headers.token;
-	}
-	return headers;
+  const token = window.localStorage.getItem("token");
+  const headers: {
+    "Content-Type": string;
+    token?: string | undefined;
+  } = {
+    "Content-Type": "application/json; charset=UTF-8",
+    token: "",
+  };
+  if (token !== undefined) {
+    headers.token = token;
+  } else {
+    delete headers.token;
+  }
+  return headers;
 }
 
 // -------------------- USERS ----------------------------
 
 export async function signin(username: string, password: string) {
-	const promise = fetch(PATH + "/users/signin", {
-		method: "POST",
-		headers: isAuth(),
-		body: JSON.stringify({
-			username,
-			password,
-		}),
-	});
-	return await promise.then(async (res) => await res.json());
+  const promise = fetch(PATH + "/users/signin", {
+    method: "POST",
+    headers: isAuth(),
+    body: JSON.stringify({
+      username,
+      password,
+    }),
+  });
+  return await promise.then(async (res) => await res.json());
 }
 
 export async function signup(
-	email: string,
-	username: string,
-	password: string
+  email: string,
+  username: string,
+  password: string
 ) {
-	const promise = fetch(PATH + "/users/signup", {
-		method: "POST",
-		headers: isAuth(),
-		body: JSON.stringify({
-			email,
-			username,
-			password,
-		}),
-	});
-	return await promise.then(async (res) => await res.json());
+  const promise = fetch(PATH + "/users/signup", {
+    method: "POST",
+    headers: isAuth(),
+    body: JSON.stringify({
+      email,
+      username,
+      password,
+    }),
+  });
+  return await promise.then(async (res) => await res.json());
 }
 
 export async function getInfo(username: string) {
-	const promise = fetch(`${PATH}/users/settings/${username}`, {
-		method: "GET",
-		headers: isAuth(),
-	});
+  const promise = fetch(`${PATH}/users/settings/${username}`, {
+    method: "GET",
+    headers: isAuth(),
+  });
 
-	return await promise.then(async (res) => await res.json());
+  return await promise.then(async (res) => await res.json());
 }
 
-export async function putInfo({ ...infos }, username: string) {
-	const promise = fetch(`${PATH}/users/settings/${username}`, {
-		method: "PUT",
-		headers: isAuth(),
-		body: JSON.stringify({ ...infos }),
-	});
-	return await promise.then(async (res) => await res.json());
+export async function putInfo(infos: object, username: string) {
+  const promise = fetch(`${PATH}/users/settings/${username}`, {
+    method: "PUT",
+    headers: isAuth(),
+    body: JSON.stringify({ ...infos }),
+  });
+
+  return await promise.then(async (res) => await res.json());
 }
 
 //  -------------------- LISTS ----------------------------
 export async function sendList({ ...lists }, username: string) {
-	const promise = fetch(`${PATH}/lists/${username}`, {
-		method: "POST",
-		headers: isAuth(),
-		body: JSON.stringify({ ...lists }),
-	});
-	return await promise.then(async (res) => await res.json());
+  const promise = fetch(`${PATH}/lists/${username}`, {
+    method: "POST",
+    headers: isAuth(),
+    body: JSON.stringify({ ...lists }),
+  });
+  return await promise.then(async (res) => await res.json());
 }
 
 export async function getAll(username: string) {
-	const promise = fetch(`${PATH}/lists/${username}`, {
-		method: "GET",
-		headers: isAuth(),
-	});
-
-	return await promise.then(async (res) => await res.json());
+  const promise = fetch(`${PATH}/lists/${username}`, {
+    method: "GET",
+    headers: isAuth(),
+  });
+  //   await promise.then(async (res: Response) => {
+  //     if (res.status === 403) {
+  //       location.href = "/403";
+  //     } else {
+  //       return await res.json();
+  //     }
+  //   });
+  // }
+  return await promise.then(async (res) => await res.json());
 }
